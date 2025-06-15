0864c047d7513705284afa5f34f9e17d0236e5e6
gowrepreya <gowrepreya@gmail.com>
2025-06-15 13:03:07 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mentorship-dashboard/',
+  base: '/mentorship-dashboard/', // This is important for GitHub Pages
   plugins: [react()],
 })
-
